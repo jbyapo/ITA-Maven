fad6a09c177d91b31401d0196a40980029285a55
YAPOJO <YAPOJO@ZHA-ITA003-W7.corp.oocl.com>
2017-08-04 16:52:21 +0800

    JUnit Testing for the 1st Week Exam

--- TestMaven/src/main/java/testMaven/testMavenClass.java
+++ TestMaven/src/main/java/testMaven/testMavenClass.java
@@ -14,6 +14,93 @@ public class testMavenClass {
 		System.out.println(division(48, 2));
 	}
 	
+	public static String number1() {
+		String result = null;
+		int i=1; 
+		for (int j=0; j<5;j=j+2) 
+		{ 
+		  i= (i*i) +j; 
+		} 
+		switch (i)  
+		{ 
+		   case 3: 
+		     //System.out.println ("zero"); 
+		     result="zero";
+		    break; 
+		   case 13: 
+		     //System.out.println ("one"); 
+		     result="one";
+		   case 175: 
+		     //System.out.println ("two"); 
+		     result="two";
+		   default: 
+		     //System.out.println ("default"); 
+		     result="default";
+		}
+		return result; 
+	}
+	
+	public static int number2() {
+		int n; 
+		int F_0=1,F_1=1; 
+		int F_n=0; 
+		System.out.println ("Enter the number"); 
+		n=5; 
+		for (int i=1; i<n; i++) { 
+		     F_n=F_0+F_1; 
+		     F_0=F_1; 
+		     F_1=F_n; 
+		} 
+		return F_n; 
+	}
+	
+	public static int number3() {
+		int inner=2, outer=2; 
+		int result=0; 
+		 
+		for (int i=0; i<outer; i++) 
+		{ 
+		  for (int j=0; j<inner; j++) 
+		      { 
+		        result = (result*result)+1; 
+		      } 
+		} 
+		
+		return result;
+	}
+	
+	public static void number4() {
+		int x;
+		int y;
+		x = 3; 
+		y = 10; 
+		while (x < y) {
+			System.out.println ("Hello"); 
+		     x = x + 1; 
+		     y = y - 1; 
+		}
+	}
+	
+	public static int number7(int integer_value) {
+		// Declare Local Variables  
+		boolean done = false;  
+		int count = 1;  
+		int current = integer_value + 2;  
+		while (! done) {  
+		  current = current / count;  
+		  if (current % 3 == 0)  
+		    done = true;  
+		  else if (current < 1)  
+		    done = true;  
+		  else  
+		    count += 1;  
+		  System.out.println(current);  
+		}  
+		System.out.println (count);  
+		return count;  
+		// method compute_value 
+	}
+	
 	public static int addition(int x, int y) {
 		return x + y;
 	}
